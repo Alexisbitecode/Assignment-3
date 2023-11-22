36bc01cccb5f8dd7d5e6c6c8299aeb1160256449
Alexisbitecode <143207524+Alexisbitecode@users.noreply.github.com>
2023-11-22 09:00:28 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,13 +26,16 @@ st.write("10. gender: Gender of the baby, female or male.")
 st.write("11. habit: Status of the mother as a nonsmoker or a smoker.")
 st.write("12. marital: Whether the mother is married or not married at birth.")
 st.write("13. whitemom: Whether the mom is white or not white.")
-st.write("We are gonna to perform an analysis on the factors related to a baby's birth weight, specifically focusing on the maternal factors.")
-st.write("Following the 'ceteris paribus' rule, we have exclusively chosen data related to male infants born to white mothers and focused our study on the factors originating from the mother that may affect the baby's birth weight.")
 
-#have some rough ideas about the dataframe
+#have some rough ideas about the dataset
 with st.expander("Click to see the top 20 rows of the data frame"):
     # Display the top 20 rows of the DataFrame
     st.dataframe(df.head(20))
+
+
+st.write("We are gonna to perform an analysis on the factors related to a baby's birth weight, specifically focusing on the maternal factors.")
+st.write("Following the 'ceteris paribus' rule, we have exclusively chosen data related to male infants born to white mothers and focused our study on the factors originating from the mother that may affect the baby's birth weight.")
+
 
 
 # Data preprocessing
@@ -48,8 +51,12 @@ filtered_df.fillna(filtered_df.mean(), inplace=True)
 # Drop unnecessary columns
 filtered_df.drop(columns=["fage", "marital", "mature", "premie", "lowbirthweight"], inplace=True)
 
-# Histograms
-st.subheader("Histograms")
+# Sidebar for data exploration
+st.sidebar.header("Data Exploration")
+
+# Histograms of each variable
+st.subheader("Histograms of Each Variable")
+st.write("Firstly, let's take a look for each varible in the dataset.")
 numeric_columns = filtered_df.select_dtypes(include=["number"]).columns
 
 # Calculate the number of rows and columns for the grid
@@ -71,7 +78,6 @@ for i, column in enumerate(numeric_columns):
     ax.set_xlabel(column)
     ax.set_ylabel("Frequency")
 
-# Remove empty subplots
 for i in range(len(numeric_columns), num_rows * num_cols):
     fig.delaxes(axes.flatten()[i])
 
@@ -80,12 +86,17 @@ plt.tight_layout()
 
 # Display the figure using st.pyplot
 st.pyplot(fig)
-st.write("From the histograms we can see...")
 
-# Sidebar for data exploration
-st.sidebar.header("Data Exploration")
+st.write("From the histograms, we observe that mothers' ages are concentrated between 20 to 35 years, with some outliers beyond 35.")
+st.write("In terms of pregnancy weeks, the majority of data points cluster around 37.5 weeks, but there are also instances beyond 35 weeks.")
+st.write("Regarding the number of hospital visits, most data falls within the range of 10 to 15 visits.")
+st.write("Mothers' weight gain during pregnancy ranges from 20 to 40 pounds.")
+st.write("For baby's birth weight, the peak occurs between 7 to 9 pounds.")
+
 
 st.subheader("Bivariate Plots")
+st.write("Now, let's explore the relationshipt bewtween each variable and baby's weight")
+
 # Create subplots with a 2-row, three-column grid
 fig, axes = plt.subplots(nrows=2, ncols=3, figsize=(15, 8))
 
@@ -128,12 +139,18 @@ plt.tight_layout()
 # Display the figure using st.pyplot
 st.pyplot(fig)
 
+st.write("From the bivariate plots, we can see that "weeks" and "weight" exhibit a clear positive relationship. Additionally, the average birth weight of babies born to non-smoking mothers is higher than that of babies born to smoking mothers.")
 
 
 # Correlation matrix
 st.subheader("Correlation Matrix")
+St.write("We want to examine the relationship of each variable with weight. Since smoking habit is a categorical variable, we will encode it as a numeric variable so that we can include it in our correlation matrix.")
+
+# One-hot encoding for habit
+filtered_df_encoded = pd.get_dummies(filtered_df, columns=["habit"], drop_first=True)
+
 # Calculate the correlation matrix
-correlation_matrix = filtered_df.corr()
+correlation_matrix = filtered_df_encoded.corr()
 # Create a heatmap of the correlation matrix
 plt.figure(figsize=(12, 8))
 sns.heatmap(correlation_matrix, annot=True, cmap="coolwarm", linewidths=0.5)
@@ -144,12 +161,12 @@ st.pyplot(plt)
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error, r2_score
+
 # Linear regression analysis with statsmodels
 st.sidebar.header("Linear Regression Analysis")
 st.write("Now, let's perform linear regression using statsmodels.")
 
-# One-hot encoding for habit
-filtered_df_encoded = pd.get_dummies(filtered_df, columns=["habit"], drop_first=True)
+
 
 # Define independent and dependent variables
 X = filtered_df_encoded[["mage", "weeks", "visits", "habit_smoker"]]
@@ -186,62 +203,17 @@ results = model_stats.fit()
 st.subheader("Regression Results")
 st.text(results.summary())
 
-# Make predictions
-y_pred = model.predict(X_test)
-
-# Calculate evaluation metrics
-mse = mean_squared_error(y_test, y_pred)
-r2 = r2_score(y_test, y_pred)
-
-# Display results
-st.write(f"Mean Squared Error (MSE): {mse}")
-st.write(f"R-squared (R2): {r2}")
-
-
-# Define independent and dependent variables
-X = filtered_df_encoded[["weeks", "habit_smoker"]]
-y = filtered_df_encoded["weight"]
-
-
-# Split the data into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
-# Create a linear regression model
-model = LinearRegression()
-
-# Train the model
-model.fit(X_train, y_train)
-
-# Make predictions on the test data
-y_pred = model.predict(X_test)
-
-# Add a constant term (intercept) to the independent variables
-X = sm.add_constant(X)
-
-# Create a linear regression model with statsmodels
-model_stats = sm.OLS(y, X)
+st.write("From the above summary, we can see that the R-squared value is 0.411, indicating that approximately 41.1% of the variance in the "weight" can be explained by the independent variables in the model.The F-statistic tests the overall significance of the regression model. In this case, it's 59.65, and the associated p-value (Prob (F-statistic)) is very close to zero (3.54e-38), indicating that the model is statistically significant.")
+st.write("We can see coefficients for the independent variables in our model. The coefficients represent the change in the dependent variable (weight) associated with a one-unit change in the corresponding independent variable while holding all other variables constant.Each coefficient also has associated statistics like standard error, t-value, and p-value. The p-values (P>|t|) are used to test the null hypothesis that the corresponding coefficient is equal to zero. In this case, "weeks" and "visits" have very low p-values, suggesting they are statistically significant predictors, while "mage" and "habit_smoker" have higher p-values, indicating they may not be as significant.")
 
 
 
-# Fit the model
-results = model_stats.fit()
-
-# Get the summary of the regression results
-
-st.text(results.summary())
-
-# Make predictions
-y_pred = model.predict(X_test)
-
-# Calculate evaluation metrics
-mse = mean_squared_error(y_test, y_pred)
-r2 = r2_score(y_test, y_pred)
-
-# Display results
 
 
-st.write(f"Mean Squared Error (MSE): {mse}")
-st.write(f"R-squared (R2): {r2}")
+
+
+
+
 
 
 
